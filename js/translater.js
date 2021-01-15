15d3b84015cfc93381e407723290092b16726300
hondrostrong-official <76270734+hondrostrong-official@users.noreply.github.com>
2021-01-15 21:19:45 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,220 @@
+var new_lang = {
+
+"lt0":  "100% natural",
+
+"lt1": "Au fost vândutе 5 000 de ambalaje",
+
+"lt2": "Peste 4 000 de recenzii",
+
+"lt3": "solicitare apel înapoi",
+
+"lt4": "Hondrostrong este o cremă contra durerii la nivelul articulațiilor, artritei și artrozei",
+
+"lt5": "comandare",
+
+"lt6": "* Numărul de ambalaje cu<span style=\"white-space: nowrap;\">reducere-<span class=\"pack_count\"></span></span>",
+
+"lt7": "<span class=\"before\"></span>Preparat din <br>midii verzi<br>extraordinare",
+
+"lt8": "<span class=\"before\"></span>Sunt produse<br>doar 1500<br>ambalaje pe an",
+
+"lt9": "<span class=\"before\"></span>Elimină<br>durerile și edemele la nivelul <br>articulațiilor",
+
+"lt10": "<span class=\"before\"></span>Reabilitează<br>articulațiile<br>și țesuturile",
+
+"lt11": "<span class=\"before\"></span>Este indicat în caz de<br>artrită<br>și artroză",
+
+"lt12": "<span class=\"before\"></span>Este eficient<br>la<br>osteocondroză",
+
+"lt13": "Ce sunt midiile verzi?",
+
+"lt14": "<span class=\"iconed gold\">Midiile verzi</span>sunt moluște care se întâlnesc pe coasta Noii Zeelande. Mediile servesc drept hrană tradițională pentru populația indigenă Maori. Reprezentanții poporului Maori, practic, nu prezintă afecțiuni ale articulațiilor și ale regiunii lombare, chiar și la o vârstă înaintată.  Cercetările efectuate au arătat că aceasta este datorată midiilor, foarte mult utilizate în rația aborigenilor.",
+
+"lt15": "Midiile conțin o cantitate foarte mare de mucopolizaharide, care în corpul uman intră în componența lichidului intercelular al țesutului conjunctiv (inclusiv al cartilajelor), datorită cărui fapt au un efect benefic asupra articulațiilor. Procedura de obținere a extractului este un proces laborios, dat fiind faptul că la prepararea acestuia nu se aplică temperaturi ridicate sau componente chimice .",
+
+"lt16": "În scurt timp a fost elaborată tehnologia de preparare a extractului din midii, care permite păstrarea proprietăților lor curative. Procedura de obținere a extractului este un proces laborios și ia mult timp, dat fiind faptul că la prepararea acestuia nu se aplică temperaturi ridicate sau componente chimice .",
+
+"lt17": "Formula cremei Hondrostrong este îmbogățită cu componente care produc un efect puternic:",
+
+"lt18": "<span class=\"iconed n2th gold\">Apitoxina (veninul albinelor)</span>- Are un efect analgezic puternic, restabilește țesutul cartilaginos lezat din articulații, declanșează procesul de regenerare a țesuturilor.",
+
+"lt19": "<span class=\"iconed n3th gold\">Suc din frunzele ilexului</span>- Tratează cu succes artritele și afecțiunile reumatice. Are un efect antiinflamator și un efect pronunțat antimicrobian față de bacteriile din genul Streptococcus, care, într-o serie de cazuri, favorizează dezvoltarea reumatismului.",
+
+"lt20": "Regenerează eficient țesutul cartilaginos lezat",
+
+"lt21": "Elimină durerile, edemele și amorțeala din articulații",
+
+"lt22": "Restabilește mobilitatea membrelor",
+
+"lt23": "Regenerează eficient țesutul cartilaginos lezat",
+
+"lt24": "Elimină durerile, edemele și amorțeala din articulații",
+
+"lt25": "Restabilește mobilitatea membrelor",
+
+"lt26": "Prin ce anume Hondrostrong este mai eficient în tratarea articulațiilor decât alte remedii?",
+
+"lt27": "În zilele noastre afecțiunile țesutului cartilaginos, precum : artritele, artrozele, poliartritele, coxartrozele, osteocondrozele, ocupă un loc însemnat la nivel mondial în ceea ce privește răspândirea în rândul populației. Spre regret, în prezent aceste patologii nu mai sunt datorate vârstei.",
+
+"lt28": "În țara noastră situația este catastrofală, circa 50% din populație au probleme legate de articulații. Dar cel mai grav este faptul că persoanele preferă metode de tratament „netradiționale”, care, de regulă, conduc la complicații. Drept rezultat, pentru evitarea agravării situației, există un remediu nou și care este cel mai eficient la ziua de azi, destinat tratării afecțiunilor țesuturilor cartilaginoase, și anume „Hondrostrong”.",
+
+"lt29": "Pentru implementarea remediului ÎN TOATE grupurile populației \"Hondrostrong\" se vinde numai prin internet, pe site-ul oficial. În acest mod se economisesc 70% din costul publicității la televizor și 300-400% de marjele farmaciilor, de aceea, absolut FIECARE persoană care are nevoie își poate permite să-și cumpere „Hondrostrong”.",
+
+"lt30": "Conform rezultatelor studiilor clinice de masă, „Hondrostrong” va elimina timp de 1,5-2 luni orice boală a țesutului cartilaginos. După performanță, remediul a depășit de 2-3 ori absolut TOATE analogurile scumpe.",
+
+"lt31": "Hondrostrong este o cremă pe baza midiilor verzi rare destinat regenerării articulațiilor",
+
+"lt32": "<span class=\"gold\">Hondrostrong</span>este preparat în baza unei rețete antice a poporului Maori, o populație indigenă din Noua Zeelandă. Rețeta se transmite din generație în generație și este ținută în mare secret. Poporul polinezian a încredințat companiei noastre rețeta cu o condiție: să producem nu mai mult de 1500 borcane de cremă pe an.",
+
+"lt33": "Componența remediului include prețiosul extract de midii verzi, care a fost îmbogățit cu apitoxină, extract de cardamom, ulei de amarant și suc din frunze de ilex.",
+
+"lt34": "În comparație cu injecțiile, la aplicare remediul nu afectează țesuturile moi și structura capsulei articulare, ci invers, pătrunde în cavitatea articulară prin osmoză și concomitent alimentează și regenerează structura și funcțiile capsulei articulare. Acest efect se realizează datorită formulei secrete a cremei \"Hondrostrong\".",
+
+"lt35": "Hondrostrong este o cremă contra durerii la nivelul articulațiilor, artritei și artrozei",
+
+"lt36": "comandare",
+
+"lt37": "* Numărul de ambalaje cu<span style=\"white-space: nowrap;\">reducere-<span class=\"pack_count\"></span></span>",
+
+"lt38": "Eficacitatea cremei Hondrostrong este demonstrată de către pacienți",
+
+"lt39": "Recomand crema “Hondrostrong” tuturor pacienților mei cu afecțiuni articulare cu origine și grad de severitate diverse. Desigur că nu toți pot să cumpere această cremă din cauza producției limitate. Crema acționează local, pătrunde direct în capsula articulară și comportă nemijlocit înlăturarea afecțiunii. Compoziția unică a remediului \"Hondrostrong\" regenerează țesutul cartilaginos lezat, elimină durerile și inflamația articulațiilor și restabilește funcționarea lor normală. Este perfect pentru tratarea osteocondrozei, artritei, artrozei, dereglării funcțiilor sistemului locomotor.",
+
+"lt40": "Andrei Cotruță",
+
+"lt41": "Chirurg de categorie superioară",
+
+"lt42": "Recomand crema “Hondrostrong” tuturor pacienților mei cu afecțiuni articulare cu origine și grad de severitate diverse. Desigur că nu toți pot să cumpere această cremă din cauza producției limitate. Crema acționează local, pătrunde direct în capsula articulară și comportă nemijlocit înlăturarea afecțiunii. Compoziția unică a remediului \"Hondrostrong\" regenerează țesutul cartilaginos lezat, elimină durerile și inflamația articulațiilor și restabilește funcționarea lor normală. Este perfect pentru tratarea osteocondrozei, artritei, artrozei, dereglării funcțiilor sistemului locomotor.",
+
+"lt43": "Andrei Cotruță",
+
+"lt44": "Chirurg de categorie superioară",
+
+"lt45": "Ce au relevat studiile clinice cu privire la remediul Hondrostrong?",
+
+"lt46": "Studiile clinice asupra remediului Hondrostrong s-au desfășurat în toamna anului 2014 în Rusia. La ele au participat mai mult de 5000 de persoane care suferă de afecțiuni articulare. Timp de 40 de zile pacienții au utilizat remediul Hondrostrong de 2 ori pe zi. Rezultatele experimentului i-au uimit chiar și pe medici!",
+
+"lt47": "Toți participanții la cercetare au notat o ameliorare însemnată în urma utilizării remediului Hondrostrong. Aproape 90% din pacienți s-au tratat de artrită. Nici un remediu  pentru tratarea afecțiunilor țesuturilor cartilaginoase și conjunctive cunoscut actualmente nu prezintă o eficacitate atât de înaltă.",
+
+"lt48": "Ce intră în zona de risc a afecțiunilor articulare?",
+
+"lt49": "<span class=\"before\"></span>Aceleași sau alte afecțiuni articulare existente",
+
+"lt50": "<span class=\"before\"></span>Ale persoanelor în etate, care au pierdut elasticitatea articulațiilor în urma modificărilor legate de vârstă",
+
+"lt51": "<span class=\"before\"></span>Care au suferit intervenții chirurgicale pe articulații",
+
+"lt52": "<span class=\"before\"></span>Care prezintă traume ale articulațiilor și în regiunea lombară",
+
+"lt53": "<span class=\"before\"></span>Care au un stil de viață sedentar (își petrec o bună parte din timp în fața computerului)",
+
+"lt54": "<span class=\"before\"></span>Care sunt supuși unei încărcături îndelungate asupra articulațiilor (inclusiv, în urma greutății excesive)",
+
+"lt55": "<span class=\"before\"></span>Sportivilor",
+
+"lt56": "<span class=\"before\"></span>Pentru prevenirea dezvoltării afecțiunilor articulare a tuturor persoanelor (dat fiind faptul că odată cu vârsta la toate persoanele articulațiile se deteriorează)",
+
+"lt57": "Recenzii ale clienților cu privire la cremă \"Hondrostrong\"",
+
+"lt58": "Anatol, 45 de ani",
+
+"lt59": "Trei ani în urmă, în februarie genunchiul meu s-a îmbolnăvit. Mai întâi mi-au făcut radiografia și au spus că am artrită. Abia-abia mergeam. Ca un invalid. Medicii mi-au propus o intervenție chirurgicală la genunchi. Nu vroiam să ajung pe masa de operație. Am început să studiez singur această problemă. Am făcut bine că nu i-am ascultat. Am aflat despre crema nouă “Hondrostrong”, am făcut o comandă pe site-ul oficial. Crema a fost livrată peste 7 zile, am aplicat-o așa cum este prevăzut în instrucțiuni. În cea de-a 11-a zi am început să mă pregătesc de maratonul municipal. Genunchiul nu mă mai supără!!",
+
+"lt60": "Ana, 30 de ani",
+
+"lt61": "Am fost internată la spital cu condroză și dureri în zona articulaţiei glenohumerale (era inflamată). M-am tratat cu picurătoarea, vitamine, magneți,  saltea (pentru regiunea lombară), acupunctură și masaj. Până la urmă, m-au externat, trebuia să mă întorc la serviciu, dar mâna mă mai durea. La o ședință neuropatologul mi-a propus ultima soluție, chiar așa a spus, să încerc un produs recent. Aplicam crema “Hondrostrong”, de 3 ori pe zi. Pe mine m-a ajutat, acum pot să lucrez liniștit și să dansez.",
+
+"lt62": "Ana, 30 de ani",
+
+"lt63": "Valentin, 62 de ani",
+
+"lt64": "Văd că nu numai mie mi-a ajutat crema “Hondrostrong”! Aceasta mă bucură. Credeam că trebuie să mă supun unei intervenții chirurgicale, dar am avut noroc de un medic bun, care mi-a prescris acest preparat. După trei tratamente articulația a fost recuperată complet, nu mai am dureri și nici edeme.  Crema este perfectă, nimic de spus.",
+
+"lt65": "Instrucțiuni de utilizare",
+
+"lt66": "Puneți o cantitate mică de cremă pe palmă",
+
+"lt67": "Aplicați cu mișcări de masaj ușoare asupra zonei afectate",
+
+"lt68": "Utilizați crema de 3 ori<span style=\"white-space: nowrap;\">pe zi</span>",
+
+"lt69": "Puneți o cantitate mică de cremă pe palmă",
+
+"lt70": "Aplicați cu mișcări de masaj ușoare asupra zonei afectate",
+
+"lt71": "Utilizați crema de 3 ori<span style=\"white-space: nowrap;\">pe zi</span>",
+
+"lt72": "Atenție! Feriți-vă de contrafaceri!",
+
+"lt73": "Midiile verzi sunt cunoscute prin proprietățile lor cuative. Crema “Hondrostrong” este preparată după o rețetă antică și secretă a popoarelor polineziene. Midiile verzi sunt recoltate numai pe coasta Noii Zeelande.",
+
+"lt74": "Feriți-vă de contrafaceri! Dacă ați văzut o cremă pe baza de midii verzi la un preț mic, cel mai probabil că este o contrafacere. În cel mai bun caz această cremă nu va avea niciun efect și în cel mai rău caz boala va avansa. Preparatul original poate fi cumpărat numai la magazinul nostru. Noi suntem furnizorii oficiali și garantăm calitatea producției.",
+
+"lt75": "Livrarea și plata:",
+
+"lt76": "<span class=\"before\"></span>Plasați comanda pe site",
+
+"lt77": "<span class=\"before\"></span>În timp de 15 zile veți primi un apel de la managerul nostru pentru confirmarea comenzii",
+
+"lt78": "<span class=\"before\"></span>Noi ambalăm și expediem comanda",
+
+"lt79": "<span class=\"before\"></span>Peste 7-14 zile comanda ajunge la oficiul poștal",
+
+"lt80": "<span class=\"before\"></span>Primiți și plătiți marfa la oficiul poștal",
+
+"lt81": "Specialiștii companiei noastre",
+
+"lt82": "Mihai Roată",
+
+"lt83": "Director calitate",
+
+"lt84": "asigură ca comanda dvs. să fie originală și ambalată corect",
+
+"lt85": "Ana Păun",
+
+"lt86": " Director call center", 
+
+"lt87": "este responsabilă de apelurile efectuate în timp util la numărul de telefon indicat de dvs. ",
+
+"lt88": "Petru Papuc",
+
+"lt89": "Director logistică",
+
+"lt90": "este responsabil de livrarea pachetului la adresa corectă și la timp",
+
+"lt91": "Tudor Captaru",
+
+"lt92": "Director calitate",
+
+"lt93": "asigură ca comanda dvs. să fie originală și ambalată corect",
+
+"lt94": "Maria Cheptene",
+
+"lt95": " Director call center", 
+
+"lt96": "este responsabilă de apelurile efectuate în timp util la numărul de telefon indicat de dvs. ",
+
+"lt97": "Alex Ermurachi",
+
+"lt98": "Director logistică",
+
+"lt99": "este responsabil de livrarea pachetului la adresa corectă și la timp",
+
+"lt100": "Hondrostrong este o cremă contra durerii la nivelul articulațiilor, artritei și artrozei",
+
+"lt101": "comandare",
+
+"lt102": "* Numărul de ambalaje cu<span style=\"white-space: nowrap;\">reducere-<span class=\"pack_count\"></span></span>",
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
